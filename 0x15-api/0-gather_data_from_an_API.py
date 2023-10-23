10b7e029cd0cf9fbd59891843e69ec472eecc784
Kibugi1 <njugunabrian843@gmail.com>
2023-10-23 10:05:23 -0700

    Correction of task 0 and 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,27 +3,15 @@
 This script returns info about an employee's todo list
 """
 import requests
-from sys import argv
+import sys
 
 
-if __name__ == '__main__':
-    url = "https://jsonplaceholder.typicode.com"
-    employeeId = argv[1]
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    employee = requests.get("{}/users/{}".format(url, employeeId)).json()
-    todos = requests.get(url + "/todos", params={"userId": employeeId}).json()
-
-    completed_tasks = []
-    for data in todos:
-        if data.get('completed') is True:
-            completed_tasks.append(data.get('title'))
-
-    employee_name = employee.get('name')
-    total_num_of_tasks = len(todos)
-    num_of_tasks_done = len(completed_tasks)
-
-    print("Employee {} is done with tasks({}/{}):".format(employee_name,
-          num_of_tasks_done, total_num_of_tasks))
-
-    for title in completed_tasks:
-        print("\t {}".format(title))
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
